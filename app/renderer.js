4d86a79412674e78b0d7de357d3213b83474b778
Tom Lewis <tom@ricochetpartners.com>
2018-11-13 13:34:58 -0800

    add jshint, basic link grabbing with localstorage

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,5 @@
+const parser = new DOMParser();
+
 const linksSection = document.querySelector('.links');
 const errorMessage = document.querySelector('.error-message');
 const newLinkForm = document.querySelector('.new-link-form');
@@ -12,3 +14,66 @@ newLinkUrl.addEventListener('keyup', () => {
 const clearForm = () => {
   newLinkUrl.value = null;
 };
+
+const parseResponse = (text) => {
+  return parser.parseFromString(text, 'text/html');
+};
+
+const findTitle = (nodes) => {
+  return nodes.querySelector('title').innerText;
+};
+
+const storeLink = (title, url) => {
+  localStorage.setItem(url, JSON.stringify({
+    title: title,
+    url: url
+  }));
+
+  console.log(localStorage);
+};
+
+const getLinks = () => {
+  return Object.keys(localStorage)
+    .map(key => JSON.parse(localStorage.getItem(key)));
+};
+
+const convertToElement = (link) => {
+  return `
+    <div class="link">
+    <h3>${link.title}</h3>
+    <p>
+    <a href="${link.url}">${link.url}</a>
+    </p>
+    </div>
+  `;
+};
+
+const renderLinks = () => {
+  const linkElements =
+    getLinks()
+      .map(convertToElement)
+      .join('');
+
+  linksSection.innerHTML = linkElements;
+};
+
+newLinkForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const url = newLinkUrl.value;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle)
+    .then(title => storeLink(title, url))
+    .then(clearForm)
+    .then(renderLinks);
+});
+
+clearStorageButton.addEventListener('click', () => {
+  localStorage.clear();
+  linksSection.innerHTML = '';
+});
+
+renderLinks();
